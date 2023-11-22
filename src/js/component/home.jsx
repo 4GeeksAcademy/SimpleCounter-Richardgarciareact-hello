7d5520021ebdab071a0f3ca25205ef27165b28c3
richardgarciaordaz <64115217+richardgarciaordaz@users.noreply.github.com>
2023-11-22 23:20:04 +0000

    Counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,17 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center row counter"  >
+	
+			<div className="col counterIcon"><i className="far fa-clock"></i></div>
+			<div className="col counterIcon">{props.six}</div>
+			<div className="col counterIcon">{props.five}</div>
+			<div className="col counterIcon">{props.four}</div>
+			<div className="col counterIcon">{props.three}</div>
+			<div className="col counterIcon">{props.two}</div>
+			<div className="col counterIcon">{props.one}</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,19 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+let seconds = 0;
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(() => {
+    const digitOne = Math.floor((seconds/1)%10)
+    const digitTwo = Math.floor((seconds/10)%10)
+    const digitThree = Math.floor((seconds/100)%10)
+    const digitFour = Math.floor((seconds/1000)%10)
+    const digitFive = Math.floor((seconds/10000)%10)
+    const digitSix = Math.floor((seconds/100000)%10)
+console.log({digitOne,digitTwo,digitThree,digitFour,digitFive,digitSix})
+    seconds +=1;
+    ReactDOM.render(<Home one={digitOne} two={digitTwo} three={digitThree} four={digitFour} five={digitFive} six={digitSix} />, document.querySelector("#app"));
+}, 1000); 
+
+
+
